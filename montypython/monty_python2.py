0323b3a678e98b30d7966b6ea83193466b39a676
Jesse McFadin <irishxpride@gmail.com>
2020-03-03 17:10:40 -0500

    debug

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -2,10 +2,13 @@
 round = 0
 answer = " "
 
-while round < 3 and answer != "Brian":
+while round < 3 and answer.lower() != "brian":
     round += 1
     answer = input("Finish the movie title, \"Monty Python\'s The Life of _____\": ")
-    if answer == 'Brian':
+    if answer.lower() == "shrubbery":
+        print ("You gave the super secret answer!")
+        break
+    if answer.lower() == 'brian':
         print('Correct!')
     elif round == 3:
         print ('Sorry, the answer was "Brian"')
