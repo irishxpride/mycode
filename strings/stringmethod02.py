8ce1951d1b2f76e84a9bfcfbd426efd78f8c8e84
Jesse McFadin <irishxpride@gmail.com>
2020-03-03 11:30:12 -0500

    backupmaker

--- strings/stringmethod02.py
+++ strings/stringmethod02.py
@@ -3,7 +3,7 @@
 def main():
 
     lilstring="Alta3 Research offers classes on Python coding"
-    newlist = lilstring.split(" ")
+    newlist = lilstring.split()
     print(newlist)
 
     myiplist = ["192", "168", "0", "12"]
