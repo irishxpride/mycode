02f194de535e1c1187b38c2a8e6252e6e68681b7
Jesse McFadin <irishxpride@gmail.com>
2020-03-03 13:19:29 -0500

    argv

--- ceph_storage/moveplease01.py
+++ ceph_storage/moveplease01.py
@@ -2,8 +2,12 @@
 
 import shutil
 import os
+import sys
 
+argv0= sys.argv[0]
+argv1= sys.argv[1]
 os.chdir('/home/student/mycode')
-shutil.move('raynor.obj', 'ceph_storage/')
-xname = input('What is the new name for kerrigan.obj? ')
-shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+#shutil.move('raynor.obj', 'ceph_storage/')
+#xname = input('What is the new name for kerrigan.obj? ')
+#shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+print(f"argv0 == {argv0}, argv1 == {argv1}")
