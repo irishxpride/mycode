52d6cb2e58648446e0516d69aebf9328e6ab13ca
Jesse McFadin <irishxpride@gmail.com>
2020-03-05 15:32:27 -0500

    catfacts

---
+++ catfacts/facts03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+
+def main():
+    r = requests.get("https://cat-fact.herokuapp.com/facts")
+
+    for catfact in r.json()["all"]:
+        print(catfact.get("text"))
+
+main()
